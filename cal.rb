7b674c9f5501e8ebb8dca77af148f0787e8d7b86
bore-ko <tori.un.sourire@gmail.com>
2022-10-10 13:06:14 +0900

    変数/メソッド名と内容を一致
    以下その他に変更をしました。
    ・hashを削除
    ・weekメソッドをmainメソッドへ移動
    ・mainメソッドの条件分岐の変更

--- cal.rb
+++ cal.rb
@@ -1,47 +1,38 @@
 require 'optparse/date'
 
-def return_the_date
-  now = Date.today
-  today =
-    { year: now.year,
-      month: now.month }
+def today
+  date = Date.today
   argv = ARGV.getopts('y:', 'm:')
   unless argv['y'].to_i.zero?
-    today[:year] = argv['y'].to_i
+    year = argv['y'].to_i
+    date = Date.new(year, date.month)
   end
   unless argv['m'].to_i.zero?
-    today[:month] = argv['m'].to_i
+    month = argv['m'].to_i
+    date = Date.new(date.year, month)
   end
-  today
+  date
 end
 
 def first_day(today)
-  Date.new(today[:year], today[:month], 1)
+  Date.new(today.year, today.month, 1)
 end
 
 def last_day(today)
-  Date.new(today[:year], today[:month], -1)
+  Date.new(today.year, today.month, -1)
 end
 
-def week(date)
-  one_week = date.strftime('%e').to_s + " "
-  if date.wday == 6
-    puts one_week
-  else
-    print one_week
-  end
-end
-
-def main
-  today = return_the_date
-  print "#{today[:month]}月 #{today[:year]}".center(20)
+def main(today)
+  print "#{today.month}月 #{today.year}".center(20)
   puts
   print %w(日 月 火 水 木 金 土).join(" ")
   puts
   print "   " * first_day(today).wday
   (first_day(today)..last_day(today)).each do |date|
-    week(date)
+    day = date.strftime('%e').to_s + " "
+    day += "\n" if date.wday == 6
+    print day
   end
 end
 
-main
+main(today)
