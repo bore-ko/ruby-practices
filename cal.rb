5735c89cb8d37d70239cd224c65d193ef4dfe168
bore-ko <tori.un.sourire@gmail.com>
2022-10-15 15:01:24 +0900

    rubocop指摘箇所の修正
    以下、修正しました。
    w%() → w%[]
    ' '  → concat

--- cal.rb
+++ cal.rb
@@ -28,13 +28,13 @@ def main
   month = calc_month
   print "#{month.month}月 #{month.year}".center(20)
   puts
-  print %w(日 月 火 水 木 金 土).join(' ')
+  print %w[日 月 火 水 木 金 土].join(' ')
   puts
   print '   ' * first_day(month).wday
   (first_day(month)..last_day(month)).each do |day|
     day_string = day.day.to_s
     day_string = "\e[30m\e[47m#{day_string}\e[0m" if day == calc_month && month.month == Date.today.month && month.year == Date.today.year
-    day_string = day_string.rjust(2) + ' '
+    day_string = day_string.rjust(2).concat(' ')
     day_string += "\n" if day.wday == 6
     print day_string
   end
