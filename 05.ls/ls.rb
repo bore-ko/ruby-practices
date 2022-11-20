ba92f863c0fa449d4eaf7a5ea58d8282b32e3180
bore-ko <tori.un.sourire@gmail.com>
2022-11-20 22:17:28 +0900

    ディレクトリ内の要素が4つの場合と、要素名が長い場合の表示の崩れを修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,33 +1,55 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def file_name
-  current_directory = Dir.entries('.')
-  file_names = []
-  current_directory.each { |f| file_names.push(f) unless f.match?(/^['.']/) }
-  file_names
+def file_or_directory_names
+  Dir.glob('*')
 end
 
-def file_blank(names)
-  names.map { |name| name.ljust(24) }
+def max_file_size(names)
+  names.map(&:size).max
 end
 
-def file_divided(names_blank)
-  into_names_blank = names_blank
-  turn_number = (into_names_blank.size / 3.0).ceil
-  file_divide = []
-  into_names_blank.sort.each_slice(turn_number) { |f| file_divide.push(f) }
-  file_divide
+def filenames_with_spaces(names, size)
+  standard_size = 25
+  small_space_size = 2
+  big_space_size = 8
+  names.map do |name|
+    if size >= standard_size
+      name.ljust(size + small_space_size)
+    else
+      name.ljust(size + big_space_size)
+    end
+  end
+end
+
+def divided_filnames(added_blank_files)
+  divisor_number = 3.0
+  turn_number = (added_blank_files.size / divisor_number).ceil
+  filenames = []
+  added_blank_files.sort.each_slice(turn_number) { |names| filenames.push(names) }
+  filenames
+end
+
+def grouped_filenames(split_filenames)
+  if split_filenames[0] && split_filenames[1] && split_filenames[2]
+    split_filenames[0].zip(split_filenames[1], split_filenames[2])
+  elsif split_filenames[1] && split_filenames[2].nil?
+    split_filenames[2] = []
+    split_filenames[2] << split_filenames[1].pop
+    split_filenames[0].zip(split_filenames[1], split_filenames[2])
+  end
 end
 
 def main
-  names = file_name
-  names_blank = file_blank(names)
-  names_divided = file_divided(names_blank)
-  if names_divided[0] && names_divided[1] && names_divided[2]
-    names_divided[0].zip(names_divided[1], names_divided[2]) { |f| puts f.join }
+  names = file_or_directory_names
+  size = max_file_size(names)
+  added_spaces_filenames = filenames_with_spaces(names, size)
+  split_filenames = divided_filnames(added_spaces_filenames)
+  processed_filenames = grouped_filenames(split_filenames)
+  if processed_filenames
+    processed_filenames.each { |f| puts f.join }
   else
-    print names_divided.join
+    print split_filenames.join
   end
 end
 
